bdde5ce71b181c1f9288d03d86deb5267c497ff9
Sudheer Ghanathe <sudheerghanathe@yahoo.com>
2020-11-28 19:40:16 -0700

    one more

--- .github/scripts/archive_app.sh
+++ .github/scripts/archive_app.sh
@@ -5,6 +5,6 @@ set -eo pipefail
 xcodebuild -workspace Calculator.xcworkspace \
             -scheme Calculator\ iOS \
             -sdk iphoneos \
-            -configuration AppStoreDistribution \
+            -configuration Release \
             -archivePath $PWD/build/Calculator.xcarchive \
             clean archive | xcpretty
